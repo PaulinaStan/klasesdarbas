9a98f0d5f4b952b18af746358d0f60639ffb6ac0
Paulina <paula232@inbox.lt>
2022-12-12 15:21:43 +0200

    mano antras komitas

---
+++ js/mano-skriptas.js
@@ -0,0 +1,25 @@
+// Pirmpji uzduotis
+let a = 1;
+console.log('kintamojo a reiksme:' + a);
+a = 2;
+console.log('kintamojo reiksme: '+ a);
+
+
+// antroji uzduotis
+let vardaspavarde ='Paulina Stanaityte';
+console.log('vardas ir pavarde: ' + vardaspavarde);
+
+
+
+// trecioji uzduotis
+let metai = "1997";
+let menesis = "06";
+let diena = "15";
+let liekanaMetai = metai % 10;
+let liekanaMenuo = menuo % 10;
+let lieknaDiena = diena %10;
+
+
+
+
+
